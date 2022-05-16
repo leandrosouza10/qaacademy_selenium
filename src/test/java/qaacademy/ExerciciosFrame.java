85adc81fda8e3b530dcfe6b975777ff0659482cb
Leandro <souzaleandro79@gmail.com>
2022-05-16 15:57:05 -0300

    Exercicio Iframe

---
+++ src/test/java/qaacademy/ExerciciosFrame.java
@@ -0,0 +1,35 @@
+package qaacademy;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExerciciosFrame {
+    WebDriver driver;
+
+    @Before
+    public void before() {
+        driver = new ChromeDriver();
+        driver.get("file:///C:/Users/Leandro/Desktop/Frames.html");
+
+    }
+
+    @Test
+    public void testIFrame() {
+        WebElement iframe =  driver.findElement(By.cssSelector("#singleframe"));
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.tagName("input")).sendKeys("TESTE IFRAME");
+
+        //https://www.selenium.dev/documentation/webdriver/browser/frames/
+    }
+
+    @After
+    public void after() throws InterruptedException {
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
